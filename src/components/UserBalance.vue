484cce1a0e84f9433037ce38cb74301f042ccdb4
saraduque <sara.duquevallejo@gmail.com>
2020-12-10 09:35:37 -0500

    jmm

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@ export default {
     this.username = this.$route.params.username;
     let self = this;
     axios
-      .get("https://cajeroapisara.herokuapp.com/user/balance/" + this.username)
+      axios.get("https://cajeroapisara.herokuapp.com/user/balance/" + this.username)
       .then((result) => {
         self.balance = result.data.balance;
       })
